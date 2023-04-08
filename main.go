65f23b1d7ed1699f91b60cbed29f77484a70a4fb
Turgay <turgaysozen@hotmail.com>
2023-04-09 00:41:28 +0300

    Add main

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var items = []int{1, 4, 7, 9, 2, 5, 3}
+	qS := quickSort(items)
+	fmt.Println("qS:", qS)
+
+	low := 0
+	high := len(qS) - 1
+	target := 1
+	bS := binarySearch(qS, target, low, high)
+	fmt.Println("bS:", bS)
+
+}
